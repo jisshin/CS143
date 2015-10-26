4028f1caaf2f76c3d4c951553ea8ce47681eb618
kudosjisoo <jshin2@caltech.edu>
2015-10-25 21:52:31 -0700

    network files

---
+++ src/network.cpp
@@ -0,0 +1,29 @@
+#include "flow.hpp"
+#include "host.hpp"
+#include "link.hpp"
+#include "router.hpp"
+
+int Network::registerFlow(char* id, FlowInfo&)
+{
+
+}
+
+int Network::registerHost(char* id)
+{
+	
+}
+
+int Network::registerLink(char* id, LinkInfo&)
+{
+	
+}
+
+int Network::registerRouter(char* id)
+{
+	
+}
+
+int Network::connectLink(char* link_id, char* node1_id, char* node2_id)
+{
+
+}
