d94fb219beae7009a5243c8b8bb53c603e4fea20
jenniferlin0902 <clin3@caltech.edu>
2015-10-25 21:32:53 -0700

    update eventqueue file name

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "EventQueue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+using std;
+
+int main(){
+	EventQueue queue1;
+	EventQueue queue2;
+	EventQueue queue3;
+	cout<<queue1.test_count<<endl;
+	cout<<queue2.test_count<<endl;
+	cout<<queue3.test_count<<endl;
+	
+}
