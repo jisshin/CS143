5054b22951c9a42eb9c796d6e88724d0de5c388e
System Administrator <c2.chenn@gmail.com>
2015-11-08 13:05:49 -0800

    new test file

---
+++ test/retrieve_network_info_test.cpp
@@ -0,0 +1,16 @@
+/**
+  * Checks the functionality of retrieving network info.
+  */
+
+#include "../include/retrieve_network_info.hpp"
+
+#include <cassert>
+#include <cstdlib>
+
+int main() {
+  RetrieveNetworkInfo retrieve;
+
+  retrieve.setNetworkInfo();
+
+
+}
