5490eb648f5dfc04c1ba5c6a8e8c144a12400390
jenniferlin0902 <clin3@caltech.edu>
2015-11-08 15:33:23 -0800

    update txevent

--- src/event/txevent.cpp
+++ src/event/txevent.cpp
@@ -30,25 +30,25 @@ int TxEvent::handleEvent(){
 	std::cout << "txevent: " << event_owner << tx_packet->packet_id << std::endl;
 #endif
 
-	//First, transmit packet
+	//First, transmit packet, and set up rx event
 	double delay = tx_node->transmitPacket(tx_packet);
 	if(delay >= 0){
 		RxEvent* next_rx = new RxEvent(tx_node->lookupRouting(event_owner)\
 				,tx_packet);
-		// TODO: update the receive event time with correct
-		// value ( according to link delay )
 		next_rx->time = time + delay;
-		eventq.push(next_rx);
+		eventq->push(next_rx);
 	}
-
-	// generate next_tx event regardless the status of
-	// transmit;
-	Packet* next_tx_packet = tx_flow->genNextPacket();
-	if (next_tx_packet != NULL){
-		TxEvent* next_tx = new TxEvent(event_owner,\
-					next_tx_packet);
-		next_tx->time = time + 1;
-		eventq.push(next_tx);
+	// if not ack packet, prepare the next tx event
+	if (tx_packet->ack_id == -1){
+		// generate next_tx event regardless the status of
+		// transmit;
+		Packet* next_tx_packet = tx_flow->genNextPacket();
+		if (next_tx_packet != NULL){
+			TxEvent* next_tx = new TxEvent(event_owner,\
+						next_tx_packet);
+			next_tx->time = tx_flow->get_tx_rate();
+			eventq->push(next_tx);
+		}
 	}
 }
 
