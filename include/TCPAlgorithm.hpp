bee6594a6c1c696001fbaf0ba70c2555ef59a1df
kudosjisoo <jshin2@caltech.edu>
2015-11-14 13:00:32 -0800

    conflict resolved

--- include/TCPAlgorithm.hpp
+++ include/TCPAlgorithm.hpp
@@ -1,17 +1,9 @@
 #ifndef TCPALGORITHM_H
 #define TCPALGORITHM_H
 
-<<<<<<< HEAD
-class TCPAlgorithm{
-
-
-};
-#endif
-=======
 class TCPAlgorithm {
 public:
 	TCPAlgorithm() {}
 	
 };
 #endif
->>>>>>> 9a560b764a97dad388e8c876803f91722918bf4e
