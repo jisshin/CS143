a80a57bbb3d9fbf5f07ae9a24f556165b01b681e
jenniferlin0902 <clin3@caltech.edu>
2015-11-15 00:32:50 -0800

    add packet type/id

--- include/packet.hpp
+++ include/packet.hpp
@@ -2,11 +2,14 @@
 #define PACKET_H
 
 #include <string>
+#include <unordered_map>
 
 const int BITS_PER_BYTE = 8;
 
 // fixed packet size in byte
-const int PACKET_SIZE = 10;
+const int SRC_PACKET = 0;
+const int ACK_PACKET = 1;
+const int ROUT_PACKET = 2;
 
 /* forward declaration class */
 class Flow; 
@@ -14,19 +17,26 @@ class Flow;
 // Assume that packet size are fix
 class Packet {
 	public:
-		Packet(std::string flow_id, std::string src, std::string dest, int id)\
+		Packet(std::string flow_id, std::string src, std::string dest, int type, int size)\
 		: packet_flow_id(flow_id),\
 		  packet_src(src), \
 		  packet_dest(dest), \
-		  packet_id(id)
+		  packet_type(type)
 		{};
 
 		std::string packet_flow_id;
 		std::string packet_src;
 		std::string packet_dest;
-		int packet_id;
-		// -1 for source packet as default
-		int ack_id = -1;
+		int packet_type;
+		int packet_size = SIZE_TABLE[packet_type];
+		int id = -1;
 };
+
+const std::unordered_map<int, int> SIZE_TABLE({
+	{SRC_PACKET, 1500},
+	{ACK_PACKET, 40},
+	{ROUT_PACKET, 40}
+});
+
 
 #endif //PACKET_H
